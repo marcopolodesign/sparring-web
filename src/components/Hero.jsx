c13a136df301001abcaa50a7251e8dbcdc1bd225
Mat Aldao <m@marcopolo.agency>
2025-02-18 10:46:25 -0300

    Pushing

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -8,10 +8,10 @@ export default function Hero({ showLoading }) {
       <div className="mx-auto max-w-3xl pt-32 sm:pt-32 lg:pt-32">
         <div className="text-center">
           <h1 className="text-5xl px-4 font-bold tracking-normal text-lightGreen uppercase font-display sm:text-8xl">
-            No te quedes con las ganas de jugar
+            Conectá con jugadores y elevá tu nivel
           </h1>
           <p className="mt-6 text-lg leading-8 text-white font-body px-4">
-            Bienvenido a la comunidad más grande de deportes de raqueta de Latinoamérica. Buscá gente con quien jugar, alquilá canchas, conseguí profesores y anotate en torneos.
+          Sparring es la plataforma definitiva para conectar jugadores, gestionar clubes y organizar torneos en un solo lugar. Buscá gente con quien jugar, alquilá canchas, conseguí profesores y anotate en torneos.
           </p>
           <div className="mt-10 flex items-center justify-center gap-x-6">
             <Button onClick={ () => {
